968a9a43f3e75229b7ac1cffaedb1b19d6b71436
LuigiPlayer <rafa.hp2006@gmail.com>
2025-12-18 12:48:19 +0000

    fix gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'LuigiPlayer1', // Usually your GitHub org/user name.
   projectName: 'TP3', // Usually your repo name.
-  deploymentBranch: "gh-pages",
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
